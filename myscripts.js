ba53de44398c25bd308f4a825e0bdf9396c9213c
adrielteng@gmail.com <adrielteng@gmail.com>
2023-11-14 18:44:19 +0800

    cache logic

--- myscripts.js
+++ myscripts.js
@@ -7,13 +7,14 @@ cache[4] = new Array();
 cache[5] = new Array();
 cache[6] = new Array();
 cache[7] = new Array();
+const age = [0,0,0,0,0,0,0,0];
 const memory = new Array();
 memory.push(new Object({block:0,value:2}));
-memory.push(new Object({block:0,value:3}));
-memory.push(new Object({block:0,value:2}));
-memory.push(new Object({block:0,value:3}));
-memory.push(new Object({block:0,value:2}));
-memory.push(new Object({block:0,value:3}));
+memory.push(new Object({block:8,value:3}));
+memory.push(new Object({block:16,value:2}));
+memory.push(new Object({block:24,value:3}));
+memory.push(new Object({block:32,value:2}));
+memory.push(new Object({block:40,value:3}));
 memory.push(new Object({block:0,value:2}));
 memory.push(new Object({block:0,value:3}));
 memory.push(new Object({block:8,value:2}));
@@ -23,29 +24,29 @@ memory.push(new Object({block:11,value:3}));
 memory.push(new Object({block:12,value:2}));
 memory.push(new Object({block:13,value:3}));
 
+
 function start(){
     while(memory.length !=0){
         curr = memory.shift();
-        curr.age = 0
         set = curr.block%8;
+        curr.age = age[set];
+        age[set]++;
         
-        index = cache[set].findIndex(e => e.value === curr.value);
+        index = cache[set].findIndex(e => e.block === curr.block);
         if (index != -1){
-            cache[set][index].age = 0;
+            cache[set][index].age = curr.age;
         }else{
-            if(cache[set].length<=4){
-                cache[set].forEach(e => {
-                    e.age++;
-                });
+            if(cache[set].length<4){
                 cache[set].push(curr);
             }else{
-                tempage = cache[set][0].age;
-                cache[set].forEach(e,i => {
-                    if(e.age<tempage){
+                tempage = 0;
+                cache[set].forEach((e,i) => {
+                    if(e.age>tempage){
                         tempage = e.age;
                         index = i;
                     }
                 });
+                cache[set][index] = curr; 
             }
         }
         // cache.forEach(e => {
